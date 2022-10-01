e3b3cb0440e053450b0c9ba4cbc9eabd0105b3cf
ThiagoGs-Dev <96384024+ThiagoGs-Dev@users.noreply.github.com>
2022-09-30 22:34:24 -0300

    New Whatsapp button update

--- app.js
+++ app.js
@@ -38,3 +38,9 @@ if (Date.now() < end) {
 }());
 
 // ====== CANVAS POSTCARD ======
+
+// ====== START Widget WhastApp hospedagemwordpresspro ======
+
+function showIt2() {document.getElementById("msg1").style.visibility = "visible";}    setTimeout("showIt2()", 5000); function hiddenIt() {document.getElementById("msg1").style.visibility = "hidden";}setTimeout("hiddenIt()", 15000); function showIt3() {document.getElementById("msg1").style.visibility = "visible";} setTimeout("showIt3()", 25000);  msg1.onclick = function() {document.getElementById('msg1').style.visibility = "hidden"; };function alertW() { document.getElementById("alertWapp").style.visibility = "visible";	} setTimeout("alertW()", 15000);
+
+// ====== END Widget WhastApp ======
